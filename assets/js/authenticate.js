b3c08f73188d33850e824a7d3c35289b980bb727
Sumit Saurav <67030264+impossibleshadowstorm@users.noreply.github.com>
2023-05-01 23:34:25 +0530

    Revert "changes"

--- assets/js/authenticate.js
+++ assets/js/authenticate.js
@@ -4,40 +4,21 @@ function loginCR() {
 
   const crRef = firebase.firestore().collection("crs");
 
-  if (uid.trim() == "") {
-    document.getElementById("invalid_msg").innerText = "Invalid UID!!";
-  } else if (pass.trim() == "") {
-    document.getElementById("invalid_msg").innerText = "Invalid Password!!";
-  } else {
-    crRef.onSnapshot((querySnap) => {
-      querySnap.docs.forEach((element) => {
-        console.log(element.data()["name"]);
-        if (element.data()["uid"] == uid) {
-          if (element.data()["pass"] == pass) {
-            location.href = "/CR.html";
-            document.getElementById("invalid_msg").innerText = "";
-            localStorage.setItem("code", "secret");
-            localStorage.setItem("uid", uid);
-            return;
-          } else {
-            document.getElementById("invalid_msg").innerText =
-              "Invalid Password!!";
-          }
-        } else {
-          // invalid user and password
-          document.getElementById("invalid_msg").innerText =
-            "No Username is registered!!";
+  crRef.onSnapshot((querySnap) => {
+    querySnap.docs.forEach((element) => {
+      console.log(element.data()["name"]);
+      if (element.data()["uid"] == uid) {
+        if (element.data()["pass"] == pass) {
+          location.href = "/Post_Notice.html";
+          return;
         }
-      });
+        else{
+            // invalid password
+        }
+      }
+      else{
+        // invalid user and password
+      }
     });
-  }
-}
-
-function loginStudents() {
-  var uid = document.getElementById("crUID").value;
-
-  // localStorage.setItem("uid", uid);
-  document.location.href = "/Student.html";
-
-  return false;
+  });
 }
