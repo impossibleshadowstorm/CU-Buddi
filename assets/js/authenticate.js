f1a7135c158356538cb32183bdf5c8dc6eec5397
singhn <singhn@unicc.org>
2023-05-01 23:47:32 +0530

    new code

--- assets/js/authenticate.js
+++ assets/js/authenticate.js
@@ -4,21 +4,40 @@ function loginCR() {
 
   const crRef = firebase.firestore().collection("crs");
 
-  crRef.onSnapshot((querySnap) => {
-    querySnap.docs.forEach((element) => {
-      console.log(element.data()["name"]);
-      if (element.data()["uid"] == uid) {
-        if (element.data()["pass"] == pass) {
-          location.href = "/Post_Notice.html";
-          return;
+  if (uid.trim() == "") {
+    document.getElementById("invalid_msg").innerText = "Invalid UID!!";
+  } else if (pass.trim() == "") {
+    document.getElementById("invalid_msg").innerText = "Invalid Password!!";
+  } else {
+    crRef.onSnapshot((querySnap) => {
+      querySnap.docs.forEach((element) => {
+        console.log(element.data()["name"]);
+        if (element.data()["uid"] == uid) {
+          if (element.data()["pass"] == pass) {
+            location.href = "/CR.html";
+            document.getElementById("invalid_msg").innerText = "";
+            localStorage.setItem("code", "secret");
+            localStorage.setItem("uid", uid);
+            return;
+          } else {
+            document.getElementById("invalid_msg").innerText =
+              "Invalid Password!!";
+          }
+        } else {
+          // invalid user and password
+          document.getElementById("invalid_msg").innerText =
+            "No Username is registered!!";
         }
-        else{
-            // invalid password
-        }
-      }
-      else{
-        // invalid user and password
-      }
+      });
     });
-  });
+  }
+}
+
+function loginStudents() {
+  var uid = document.getElementById("crUID").value;
+
+  // localStorage.setItem("uid", uid);
+  document.location.href = "/Student.html";
+
+  return false;
 }
--- assets/js/script_Dashboard.js
+++ assets/js/script_Dashboard.js
@@ -1,87 +1,86 @@
 "use strict";
 $(function () {
-    $(".menu-link").click(function () {
-        $(".menu-link").removeClass("is-active");
-        $(this).addClass("is-active");
-    });
+  $(".menu-link").click(function () {
+    $(".menu-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
 });
 $(function () {
-    $(".main-header-link").click(function () {
-        $(".main-header-link").removeClass("is-active");
-        $(this).addClass("is-active");
-    });
+  $(".main-header-link").click(function () {
+    $(".main-header-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
 });
 const dropdowns = document.querySelectorAll(".dropdown");
 dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener("click", (e) => {
-        e.stopPropagation();
-        dropdowns.forEach((c) => c.classList.remove("is-active"));
-        dropdown.classList.add("is-active");
-    });
+  dropdown.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdowns.forEach((c) => c.classList.remove("is-active"));
+    dropdown.classList.add("is-active");
+  });
 });
 $(".search-bar input")
-    .focus(function () {
+  .focus(function () {
     $(".header").addClass("wide");
-})
-    .blur(function () {
+  })
+  .blur(function () {
     $(".header").removeClass("wide");
-});
+  });
 $(document).click(function (e) {
-    var container = $(".status-button");
-    var dd = $(".dropdown");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-        dd.removeClass("is-active");
+  var container = $(".status-button");
+  var dd = $(".dropdown");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    dd.removeClass("is-active");
+  }
+});
+$(function () {
+  $(".dropdown").on("click", function (e) {
+    $(".content-wrapper").addClass("overlay");
+    e.stopPropagation();
+  });
+  $(document).on("click", function (e) {
+    if ($(e.target).is(".dropdown") === false) {
+      $(".content-wrapper").removeClass("overlay");
     }
+  });
 });
 $(function () {
-    $(".dropdown").on("click", function (e) {
-        $(".content-wrapper").addClass("overlay");
-        e.stopPropagation();
-    });
-    $(document).on("click", function (e) {
-        if ($(e.target).is(".dropdown") === false) {
-            $(".content-wrapper").removeClass("overlay");
-        }
-    });
-});
-$(function () {
-    $(".status-button:not(.open)").on("click", function (e) {
-        $(".overlay-app").addClass("is-active");
-    });
-    $(".pop-up .close").click(function () {
-        $(".overlay-app").removeClass("is-active");
-    });
+  $(".status-button:not(.open)").on("click", function (e) {
+    $(".overlay-app").addClass("is-active");
+  });
+  $(".pop-up .close").click(function () {
+    $(".overlay-app").removeClass("is-active");
+  });
 });
 $(".status-button:not(.open)").click(function () {
-    $(".pop-up").addClass("visible");
+  $(".pop-up").addClass("visible");
 });
 $(".pop-up .close").click(function () {
-    $(".pop-up").removeClass("visible");
+  $(".pop-up").removeClass("visible");
 });
-const toggleButton = document.querySelector('.dark-light');
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode');
+const toggleButton = document.querySelector(".dark-light");
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 });
 
-
-//It will show only coressponding Data related to field 
+//It will show only coressponding Data related to field
 $("#Worksheet-only").click(function () {
-    $("#Assignment-data").hide();
-    $("#Worksheet-data").show();
-    $("#Announcement-data").hide();
+  $("#Assignment-data").hide();
+  $("#Worksheet-data").show();
+  $("#Announcement-data").hide();
 });
 $("#Assignment-only").click(function () {
-    $("#Worksheet-data").hide();
-    $("#Assignment-data").show();
-    $("#Announcement-data").hide();
+  $("#Worksheet-data").hide();
+  $("#Assignment-data").show();
+  $("#Announcement-data").hide();
 });
 $("#Announcement-only").click(function () {
-    $("#Assignment-data").hide();
-    $("#Announcement-data").show();
-    $("#Worksheet-data").hide();
+  $("#Assignment-data").hide();
+  $("#Announcement-data").show();
+  $("#Worksheet-data").hide();
 });
 $("#All-data").click(function () {
-    $("#Assignment-data").show();
-    $("#Announcement-data").show();
-    $("#Worksheet-data").show();
+  $("#Assignment-data").show();
+  $("#Announcement-data").show();
+  $("#Worksheet-data").show();
 });
