a0cffe677396c3649fba1d8e65f84eb8eb1b214f
singhn <singhn@unicc.org>
2023-05-05 00:42:59 +0530

    deletion done

--- assets/js/cr_dashboard.js
+++ assets/js/cr_dashboard.js
@@ -2,6 +2,7 @@ var uid = localStorage.getItem("uid");
 document.getElementById("crid").innerText = uid;
 
 getAllTiles("worksheets");
+var check_menu = 0;
 
 function createThread() {
   if (document.getElementById("wks-radio").checked) {
@@ -18,9 +19,8 @@ function createThread() {
         subject: document.getElementById("wks-subject").value,
         wks_no: document.getElementById("wks-no").value,
       });
-      console.log("done");
-  }
-  else if (document.getElementById("assignment-radio").checked) {
+    console.log("done");
+  } else if (document.getElementById("assignment-radio").checked) {
     firebase
       .firestore()
       .collection("crs")
@@ -34,9 +34,8 @@ function createThread() {
         subject: document.getElementById("wks-subject").value,
         assignment_no: document.getElementById("wks-no").value,
       });
-      console.log("done");
-  }
-  else if (document.getElementById("announcement-radio").checked) {
+    console.log("done");
+  } else if (document.getElementById("announcement-radio").checked) {
     firebase
       .firestore()
       .collection("crs")
@@ -48,9 +47,9 @@ function createThread() {
           new Date(document.getElementById("wks-dead-date").value)
         ),
         notice_heading: document.getElementById("wks-notice-heading").value,
-        notice: document.getElementById("about").value
+        notice: document.getElementById("about").value,
       });
-      console.log("done");
+    console.log("done");
   }
 }
 
@@ -64,21 +63,21 @@ function getAllTiles(threadType) {
   document.getElementById("wks-ongoing").innerHTML = "";
 
   if (threadType == "worksheets") {
+    check_menu = 0;
     crRef.onSnapshot((querySnap) => {
       querySnap.docs.forEach((element) => {
+        //Date Calculation Start
+        const timestamp = element.data()["deadline"].seconds;
+        const now1 = Date.now();
 
-    //Date Calculation Start
-    const timestamp = element.data()["deadline"].seconds; 
-    const now1=Date.now();
-    
-    const now = Math.floor(now1/1000);
-  
-    const diff =timestamp-now;
-    const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60 )));
-   
-    //Date Calculation End
-      
-        var child = `<li class="adobe-product" id="Worksheet-data">
+        const now = Math.floor(now1 / 1000);
+
+        const diff = timestamp - now;
+        const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60)));
+
+        //Date Calculation End
+
+        var child = `<li class="adobe-product" id="${element.id}">
     <div class="products">
       <span id="Subject">${element.data()["subject"]}</span>
     </div>
@@ -87,32 +86,32 @@ function getAllTiles(threadType) {
       <span>Worksheet ${element.data()["wks_no"]}</span> </span>
     <div class="button-wrapper">
       <button class="content-button status-button open"><span>${diffInDays} </span>Days Left</button>
-      <div class="menu">
-        <button class="dropdown">
-          <ul>
-            <li><a href="#">Update</a></li>
-            <li><a href="#">Delete</a></li>
-          </ul>
-        </button>
-      </div>
+      &nbsp;&nbsp;&nbsp;
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+      width="24" height="24"
+      onclick="deleteCard(${element.id})"
+      viewBox="0,0,256,256"
+      style="fill:#000000;">
+      <g fill="#ff0000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M10,2l-1,1h-6v2h18v-2h-6l-1,-1zM4.36523,7l1.70313,15h11.86328l1.70313,-15z"></path></g></g>
+      </svg>
     </div>
   </li>`;
         document.getElementById("wks-ongoing").innerHTML += child;
       });
     });
   } else if (threadType == "assignments") {
+    check_menu = 1;
     crRef.onSnapshot((querySnap) => {
       querySnap.docs.forEach((element) => {
-
         //Date Calculation Start
-    const timestamp = element.data()["deadline"].seconds; 
-    
-    const now = Math.floor(Date.now()/1000);
-    const diff =now-timestamp;
-    const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60 )));
-    //Date Calculation End
+        const timestamp = element.data()["deadline"].seconds;
 
-        var child = `<li class="adobe-product" id="Worksheet-data">
+        const now = Math.floor(Date.now() / 1000);
+        const diff = now - timestamp;
+        const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60)));
+        //Date Calculation End
+
+        var child = `<li class="adobe-product">
     <div class="products">
       <span id="Subject">${element.data()["subject"]}</span>
     </div>
@@ -121,6 +120,15 @@ function getAllTiles(threadType) {
       <span>Assignment ${element.data()["assignment_no"]}</span> </span>
       <div class="button-wrapper">
       <button class="content-button status-button open"><span>${diffInDays} </span>Days Left</button>
+      &nbsp;&nbsp;&nbsp;
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+      id="${element.id}"
+      onclick="deleteCard(${element.id})"
+      width="24" height="24"
+      viewBox="0,0,256,256"
+      style="fill:#000000;">
+      <g fill="#ff0000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M10,2l-1,1h-6v2h18v-2h-6l-1,-1zM4.36523,7l1.70313,15h11.86328l1.70313,-15z"></path></g></g>
+      </svg>
     </div>
   </li>`;
         document.getElementById("wks-ongoing").innerHTML += child;
@@ -130,24 +138,32 @@ function getAllTiles(threadType) {
     // document.getElementById("wks-ongoing").innerHTML = finalList;
     // console.log(finalList)
   } else if (threadType == "announcements") {
+    check_menu = 2;
     crRef.onSnapshot((querySnap) => {
       querySnap.docs.forEach((element) => {
-
         //Date Calculation Start
-    const timestamp = element.data()["deadline"].seconds; 
-    
-    const now = Math.floor(Date.now()/1000);
-    const diff =now-timestamp;
-    const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60 )));
-   
-    //Date Calculation End
+        const timestamp = element.data()["deadline"].seconds;
 
-        var child = `<li class="adobe-product" id="Worksheet-data">
+        const now = Math.floor(Date.now() / 1000);
+        const diff = now - timestamp;
+        const diffInDays = Math.abs(Math.floor(diff / (24 * 60 * 60)));
+
+        //Date Calculation End
+
+        var child = `<li class="adobe-product">
     <div class="products">
       <span id="Subject">${element.data()["notice_heading"]}</span>
     </div>
       <div class="button-wrapper">
       <button class="content-button status-button open"><span> ${diffInDays} </span>Days Left</button>
+      &nbsp;&nbsp;&nbsp;
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+      onclick="deleteCard(${element.id})"
+      width="24" height="24"
+      viewBox="0,0,256,256"
+      style="fill:#000000;">
+      <g fill="#ff0000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M10,2l-1,1h-6v2h18v-2h-6l-1,-1zM4.36523,7l1.70313,15h11.86328l1.70313,-15z"></path></g></g>
+      </svg>
     </div>
   </li>`;
         document.getElementById("wks-ongoing").innerHTML += child;
@@ -156,34 +172,73 @@ function getAllTiles(threadType) {
   }
 }
 
-// const crRef = firebase
-//   .firestore()
-//   .collection("crs")
-//   .doc(uid)
-//   .collection("worksheets");
+async function deleteCard(id) {
+  if (check_menu == 0) {
+        await firebase
+          .firestore()
+          .collection("crs")
+          .doc(uid)
+          .collection("worksheets")
+          .doc(id.id)
+          .delete();
+        getAllTiles("worksheets");
+      } else if (check_menu == 1) {
+        await firebase
+          .firestore()
+          .collection("crs")
+          .doc(uid)
+          .collection("assignments")
+          .doc(id.id)
+          .delete();
+        getAllTiles("assignments");
+      } else if (check_menu == 2) {
+        await firebase
+          .firestore()
+          .collection("crs")
+          .doc(uid)
+          .collection("announcements")
+          .doc(id.id)
+          .delete();
+        getAllTiles("announcements");
+      }
+    
+}
 
-// crRef.onSnapshot((querySnap) => {
-//   querySnap.docs.forEach((element) => {
-//     console.log(element.data());
-//     var child = `<li class="adobe-product" id="Worksheet-data">
-//       <div class="products">
-//         <span id="Subject">${element.data()["subject"]}</span>
-//       </div>
-//       <span class="status">
-//         <span class="status-circle green"></span>
-//         <span>Worksheet ${element.data()["wks_no"]}</span> </span>
-//       <div class="button-wrapper">
-//         <button class="content-button status-button open"><span>7 </span>Days Left</button>
-//         <div class="menu">
-//           <button class="dropdown">
-//             <ul>
-//               <li><a href="#">Update</a></li>
-//               <li><a href="#">Delete</a></li>
-//             </ul>
-//           </button>
-//         </div>
-//       </div>
-//     </li>`;
-//     document.getElementById("wks-ongoing").innerHTML +=child;
-//   });
-// });
+// function getEventTarget(e) {
+//   e = e || window.event;
+//   return e.target || e.srcElement;
+// }
+
+// var ul = document.getElementById("wks-ongoing");
+// ul.onclick = function (event) {
+//   var target = getEventTarget(event);
+//   if (check_menu == 0) {
+//     firebase
+//       .firestore()
+//       .collection("crs")
+//       .doc(uid)
+//       .collection("worksheets")
+//       .doc(target.id)
+//       .delete();
+//     getAllTiles("worksheets");
+//   } else if (check_menu == 1) {
+//     firebase
+//       .firestore()
+//       .collection("crs")
+//       .doc(uid)
+//       .collection("assignments")
+//       .doc(target.id)
+//       .delete();
+//     getAllTiles("assignments");
+//   } else if (check_menu == 2) {
+//     firebase
+//       .firestore()
+//       .collection("crs")
+//       .doc(uid)
+//       .collection("announcements")
+//       .doc(target.id)
+//       .delete();
+//     getAllTiles("announcements");
+//   }
+//   console.log(target.id);
+// };
